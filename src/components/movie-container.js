9d9e9eea6f21c2d470ee9b725e0bafba7e6758ac
Secret <toontown@inbox.ru>
2019-09-27 23:56:46 +0300

    module5-task2

--- src/components/movie-container.js
+++ src/components/movie-container.js
@@ -1,7 +1,6 @@
 import AbstractComponent from './abstract-component.js';
 
 export default class MovieContainer extends AbstractComponent {
-
   getTemplate() {
     return `<section class="films">
     </section>`;
