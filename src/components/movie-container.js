f2fa86b4de83dad672b373693a847cab38d0bf65
Secret <toontown@inbox.ru>
2019-10-01 22:41:25 +0300

    16. Личный проект: запомни и пересчитай

--- src/components/movie-container.js
+++ src/components/movie-container.js
@@ -4,11 +4,6 @@ export default class MovieContainer extends AbstractComponent {
   constructor() {
     super();
   }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> module5-task2
   getTemplate() {
     return `<section class="films">
     </section>`;
